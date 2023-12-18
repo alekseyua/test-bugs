c24ffe350c79ffa2efcf55c5e9293a15f84ea375
Alekseyua <alekseyuadnepr@gmail.com>
2023-12-18 23:17:19 +0200

    fix bug v-5

--- src/Pages/TextareaScreen/TextareaScreen.js
+++ src/Pages/TextareaScreen/TextareaScreen.js
@@ -6,7 +6,7 @@ export default function TextareaScreen() {
   const navigate = useNavigate();
   useEffect(()=>{
     tg.BackButton.show()
-    tg.BackButton.onClick(navigate('/main')) 
+    tg.BackButton.onClick( () => navigate('/main')) 
   },[])
   return (
     <div className={['container mt-8 grid grid-cols-1 justify-items-center']}>
