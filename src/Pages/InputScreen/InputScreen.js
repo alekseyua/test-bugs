31e7600735e779392c6dcc51bf86fca6886e8f27
Alekseyua <alekseyuadnepr@gmail.com>
2023-12-18 23:14:47 +0200

    fix bug v-4

--- src/Pages/InputScreen/InputScreen.js
+++ src/Pages/InputScreen/InputScreen.js
@@ -6,7 +6,7 @@ export default function InputScreen() {
   const navigate = useNavigate();
   useEffect(()=>{
     tg.BackButton.show()
-    tg.BackButton.onClick(()=>navigate('/test/main'))
+    tg.BackButton.onClick(()=>navigate('/main'))
   },[])
   return (
     <div className={['container mt-8 grid grid-cols-1 justify-items-center']}>
